9f5ec3fbfbdd7d1d2ad7e4459223715da543fbcb
RISHABH CHHAPARIA <RISHABH CHHAPARIA>
2017-05-25 20:33:45 +0530

    bookmark added

--- client/components/cardDescription/description.js
+++ client/components/cardDescription/description.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Chip from 'material-ui/Chip';
+import RaisedButton from 'material-ui/RaisedButton';
 import FlatButton from 'material-ui/FlatButton';
+import IconButton from 'material-ui/IconButton';
+import ActionGrade from 'material-ui/svg-icons/action/grade';
 import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card';
 import Badge from 'material-ui/Badge';
 import cookie from 'react-cookie';
@@ -8,12 +11,12 @@ import cookie from 'react-cookie';
 class CardDescription extends React.Component {
     constructor(props) {
         super(props);
-        var currCard= cookie.load('currentCard');
+        var currCard = cookie.load('currentCard');
         this.state = {
             card: currCard,
             likes: 0 | this.props.likedPosts.get(currCard.id),
             dislikes: 0 | this.props.dislikedPosts.get(currCard.id),
-            bookmarked: false | this.props.bookmarkedPosts.get(currCard.id)
+            bookmarked: false | this.props.bookmarkedPosts.get(currCard.id),
         }
     }
 
@@ -32,38 +35,47 @@ class CardDescription extends React.Component {
         }
     }
     render() {
+        let bm_flag;
+        if(this.state.bookmarked)
+        bm_flag=1;
+        else
+        bm_flag=0;
         const styles = {
-            overflow: 'auto',
-            height: 500,
-            margin: 20,
-            padding: 20,
-            textAlign: 'center',
-            position: 'relative',
+            marginTop: -50,
+            marked: {
+                color: 'yellow'
+            },
+            unmarked: {
+                color: 'black'
+            }
         };
 
         return (
             <div className="container">
-                <h3>{this.state.card.title}</h3>
-                    <img src={this.state.card.thumbnailUrl} width="150" height="250" className="col-xs-12 col-sm-4" alt="" />
-                    <p className="cardDescription col-xs-12 col-sm-8">
-                        {this.state.card.body}
-                    </p>
-                    <CardActions className="col-md-offset-6 cardAction">
-                        <FlatButton label="Like" primary={true} onClick={this.update.bind(this, "like")} />
-                        <Badge
-                            badgeContent={this.state.likes}
-                            primary={true}
-                        >
-                        </Badge>
+                <h1>{this.state.card.title}</h1>
+                <img src={this.state.card.thumbnailUrl} width="150" height="250" className="col-xs-12 col-sm-4" alt="" />
+                <p className="cardDescription col-xs-12 col-sm-8">
+                    {this.state.card.body}
+                </p>
+                <CardActions className="col-md-offset-6 cardAction">
+                    <RaisedButton label="Like" primary={true} onClick={this.update.bind(this, "like")} />
+                    <Badge
+                        badgeContent={this.state.likes}
+                        primary={true}
+                    >
+                    </Badge>
 
-                        <FlatButton label="Dislike" secondary={true} onClick={this.update.bind(this, "dislike")} />
-                        <Badge
-                            badgeContent={this.state.dislikes}
-                            secondary={true}
-                        >
-                        </Badge>
-
-                    </CardActions>
+                    <RaisedButton label="Dislike" secondary={true} onClick={this.update.bind(this, "dislike")} />
+                    <Badge
+                        badgeContent={this.state.dislikes}
+                        secondary={true}
+                    >
+                    </Badge>
+                   {(bm_flag) ? <FlatButton label="Bookmark" onClick={this.update.bind(this, "bookmark")}><span className="glyphicon glyphicon-bookmark pull-right" style={styles.marked}></span></FlatButton>
+                            :
+                            <FlatButton label="Bookmark" onClick={this.update.bind(this, "bookmark")}><span className="glyphicon glyphicon-bookmark pull-right" style={styles.unmarked}></span></FlatButton>
+                            }
+                </CardActions>
             </div>
         );
     }
--- client/components/cardList/cardDetails.js
+++ client/components/cardList/cardDetails.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
+import IconButton from 'material-ui/IconButton';
+import ActionGrade from 'material-ui/svg-icons/action/grade';
 import Badge from 'material-ui/Badge';
 import { hashHistory } from 'react-router';
 import cookie from 'react-cookie';
@@ -33,7 +36,7 @@ class CardDetails extends React.Component {
     }
     details() {
         this.props.currentCard(this.props.card);
-        cookie.save('currentCard',this.props.card);
+        cookie.save('currentCard', this.props.card);
         hashHistory.push(`/details/${this.props.card.id}`);
     }
     truncate(str) {
@@ -42,6 +45,11 @@ class CardDetails extends React.Component {
     render() {
         var description;
         let flag;
+        let bm_flag;
+        if(this.state.bookmarked)
+        bm_flag=1;
+        else
+        bm_flag=0;
         if (this.props.card.body.length > 256) {
             flag = 1
             description = this.truncate(this.props.card.body);
@@ -51,21 +59,26 @@ class CardDetails extends React.Component {
             description = this.props.card.body;
         }
 
-
         const styles = {
             bookmark: {
                 marginBottom: 16,
-                marginLeft:15
+                marginLeft: 15
             },
-            card:{
-                overflow:'auto',
-                height:580,
-                position:'relative'
+            card: {
+                overflow: 'auto',
+                height: 580,
+                position: 'relative'
             },
-            cardAction:{
-                marginTop:'15px',
-                position:'absolute',
-                bottom:'10px'
+            cardAction: {
+                marginTop: '15px',
+                position: 'absolute',
+                bottom: '10px'
+            },
+            marked: {
+                color: 'yellow'
+            },
+            unmarked: {
+                color: 'black'
             }
         };
         return (
@@ -73,12 +86,7 @@ class CardDetails extends React.Component {
                 <Card
                     style={styles.card}
                     onClick={this.details.bind(this)}>
-                    <CardHeader
-                        onClick={this.details.bind(this)}
-                        title={this.state.card.title}
-                        actAsExpander={false}
-                        showExpandableButton={false}
-                    />
+                    <h2>{this.state.card.title}</h2>
                     <img src={this.props.card.thumbnailUrl} width="50px" height="200px" className="cardImg col-xs-11" />
                     {(flag) ?
                         <CardText expandable={false} className="col-xs-12">
@@ -92,20 +100,26 @@ class CardDetails extends React.Component {
                     }
                     <CardActions style={styles.cardAction} className="cardAction">
                         <span>
-                        <FlatButton label="Like" primary={true} onClick={this.update.bind(this, "like")} />
-                        <Badge
-                            badgeContent={this.state.likes}
-                            primary={true}
-                        >
-                        </Badge>
+                            <RaisedButton label="Like" primary={true} onClick={this.update.bind(this, "like")} />
+                            <Badge
+                                badgeContent={this.state.likes}
+                                primary={true}
+                            >
+                            </Badge>
                         </span>
                         <span>
-                        <FlatButton label="Dislike" secondary={true} onClick={this.update.bind(this, "dislike")} />
-                        <Badge
-                            badgeContent={this.state.dislikes}
-                            secondary={true}
-                        >
-                        </Badge>
+                            <RaisedButton label="Dislike" secondary={true} onClick={this.update.bind(this, "dislike")} />
+                            <Badge
+                                badgeContent={this.state.dislikes}
+                                secondary={true}
+                            >
+                            </Badge>
+                        </span>
+                        <span>
+                            {(bm_flag) ? <FlatButton label="Bookmark" onClick={this.update.bind(this, "bookmark")}><span className="glyphicon glyphicon-bookmark pull-right" style={styles.marked}></span></FlatButton>
+                            :
+                            <FlatButton label="Bookmark" onClick={this.update.bind(this, "bookmark")}><span className="glyphicon glyphicon-bookmark pull-right" style={styles.unmarked}></span></FlatButton>
+                            }
                         </span>
                     </CardActions>
 
